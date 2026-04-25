e598664c2df8868527da78248884ffc87b96649e
sammyXXIV <samsonsamuel531@gmail.com>
2026-04-25 22:23:48 +0100

    Refactor createInstance to use SepoliaConfig
    Updated the createInstance function to include SepoliaConfig and changed the network URL.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { createInstance } = require('@zama-fhe/relayer-sdk/node');
+const { createInstance, SepoliaConfig } = require('@zama-fhe/relayer-sdk/node');
 
 const app = express();
 app.use(cors());
@@ -15,15 +15,8 @@ app.post('/encrypt', async (req, res) => {
 
   try {
     const instance = await createInstance({
-      aclContractAddress: '0x687820221192C5B662b25367F70076A37bc79b6c',
-      kmsContractAddress: '0x1364cBBf2cDF5032C47d8226a6f6FBD2AFCDacAC',
-      inputVerifierContractAddress: '0xbc91f3daD1A5F19F8390c400196e58073B6a0BC4',
-      verifyingContractAddressDecryption: '0xb6E160B1ff80D67Bfe90A85eE06Ce0A2613607D1',
-      verifyingContractAddressInputVerification: '0x7048C39f048125eDa9d678AEbaDfB22F7900a29F',
-      chainId: 11155111,
-      gatewayChainId: 55815,
-      relayerUrl: 'https://relayer.testnet.zama.cloud',
-      network: 'https://ethereum-sepolia-rpc.publicnode.com',
+      ...SepoliaConfig,
+      network: 'https://eth-sepolia.public.blastapi.io',
     });
 
     const encrypted = await instance
